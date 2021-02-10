dc9887c981b387fde805ec789db454dae4fd6295
speltriao <38846168+speltriao@users.noreply.github.com>
2021-02-09 23:38:49 -0300

    Update and rename lembro.c to array_malloc.c

--- array_malloc.c
+++ array_malloc.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-
-    
     int n;
     printf("Digite o tamanho do vetor");
     scanf("%d",&n);
     int *vetor= malloc(n*sizeof(int));
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         scanf("%d",(vetor+i));
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         printf("[%d]",*(vetor+i));
     }
-    
     return 0;
 }
